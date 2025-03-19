02f16d6adbac883fd75326fde68341b397f55a05
Marino Michael <marino.michael.1990@gmail.com>
2025-03-19 17:16:44 +0100

    add auth

---
+++ src/app/(home)/protected/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div className="">only logged</div>;
+};
+
+export default Page;
--- src/modules/home/ui/components/home-sidebar/personal-section.tsx
+++ src/modules/home/ui/components/home-sidebar/personal-section.tsx
@@ -10,6 +10,7 @@ import {
 } from '@/components/ui/sidebar';
 import { HistoryIcon, ListVideoIcon, ThumbsUpIcon } from 'lucide-react';
 import Link from 'next/link';
+import { SignedIn, useAuth, useClerk } from '@clerk/nextjs';
 
 const items = [
   {
@@ -33,28 +34,37 @@ const items = [
 ];
 
 export const PersonalSection = () => {
+  const clerk = useClerk();
+  const { isSignedIn } = useAuth();
   return (
-    <SidebarGroup>
-      <SidebarGroupLabel>You</SidebarGroupLabel>
-      <SidebarGroupContent>
-        <SidebarMenu>
-          {items.map((item) => (
-            <SidebarMenuItem key={item.title}>
-              <SidebarMenuButton
-                tooltip={item.title}
-                asChild
-                isActive={false} // todo : change to look at current pathname
-                onClick={() => {}} // todo : to something when click
-              >
-                <Link href={item.url} className="flex items-center gap-4">
-                  <item.icon />
-                  <span className="text-sm">{item.title}</span>
-                </Link>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-          ))}
-        </SidebarMenu>
-      </SidebarGroupContent>
-    </SidebarGroup>
+    <SignedIn>
+      <SidebarGroup>
+        <SidebarGroupLabel>You</SidebarGroupLabel>
+        <SidebarGroupContent>
+          <SidebarMenu>
+            {items.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton
+                  tooltip={item.title}
+                  asChild
+                  isActive={false} // todo : change to look at current pathname
+                  onClick={(e) => {
+                    if (!isSignedIn && item.auth) {
+                      e.preventDefault();
+                      return clerk.openSignIn();
+                    }
+                  }}
+                >
+                  <Link href={item.url} className="flex items-center gap-4">
+                    <item.icon />
+                    <span className="text-sm">{item.title}</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarGroup>
+    </SignedIn>
   );
 };
